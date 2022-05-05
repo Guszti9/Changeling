1fd1477386df3b73766cd47cec0e6fb194afe26a
Guszti9 <tusi0531@gmail.com>
2022-05-05 13:50:29 +0200

    make skill and attribute value changeable

--- static/js/characterSheet.js
+++ static/js/characterSheet.js
@@ -1,16 +1,43 @@
 function addEventListenerToAttribute() {
     const attributeFavicon = [...document.querySelectorAll(".attribute-favicon")];
     for (let favicon of attributeFavicon) {
-        favicon.addEventListener("click", (e) => {
-            let target = e.currentTarget
-            target.classList.toggle("fa-circle");
-            target.classList.toggle("fa-times-circle");
-        })
+        favicon.addEventListener("click", clickOnIcon)
+    }
+}
+
+function addEventListenerToSkill() {
+    const attributeFavicon = [...document.querySelectorAll(".skill-favicon")];
+    for (let favicon of attributeFavicon) {
+        favicon.addEventListener("click", clickOnIcon)
+    }
+}
+
+function clickOnIcon(e) {
+    let target = e.currentTarget;
+    let clickValue = target.getAttribute("data-ind");
+    let attrName = target.parentElement.getAttribute("data-name");
+    let attrIcons = target.parentElement.children;
+
+    let attrValue;
+
+    if (target.classList.contains("fa-circle")) {
+        attrValue = clickValue;
+        for (let i = 0; i < clickValue; i++) {
+            attrIcons[i].classList.add("fa-times-circle");
+            attrIcons[i].classList.remove("fa-circle");
+        }
+    } else {
+        attrValue = clickValue - 1;
+        for (let i = clickValue - 1; i < 5; i++) {
+            attrIcons[i].classList.add("fa-circle");
+            attrIcons[i].classList.remove("fa-times-circle");
+        }
     }
 }
 
 function init() {
     addEventListenerToAttribute();
+    addEventListenerToSkill();
 }
 
 init();
