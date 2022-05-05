95854901f9cb0fe2746a6a53cbc181dff3bb221b
Guszti9 <tusi0531@gmail.com>
2022-05-05 10:32:01 +0200

    refactor sheet_controller to give back new data format

--- sheet_controller.py
+++ sheet_controller.py
@@ -1,12 +1,18 @@
 import queires
 
+types = ["Mental", "Physical", "Social"]
+
 mental_attributes = ["Intelligence", "Wits", "Resolve"]
 physical_attributes = ["Strength", "Dexterity", "Stamina"]
 social_attributes = ["Presence", "Manipulation", "Composure"]
 
+attribute_names = [mental_attributes, physical_attributes, social_attributes]
+
 mental_skills = ["Academics", "Computer", "Crafts", "Investigation", "Medicine", "Occult", "Politics", "Science"]
 physical_skills = ["Athletics", "Brawl", "Drive", "Firearms", "Larceny", "Stealth", "Survival", "Weaponry"]
 social_skills = ["Animal Ken", "Empathy", "Expression", "Intimidation", "Persuasion", "Socialize", "Streetwise", "Subterfuge"]
+
+skill_names = [mental_skills, physical_skills, social_skills]
 
 
 def get_sheet(id):
@@ -17,8 +23,8 @@ def get_sheet(id):
     skills = get_skills(sheet_data["skills"])
 
     sheet_dictionary["id"] = sheet_data["id"]
-    sheet_dictionary.update(attributes)
-    sheet_dictionary.update(skills)
+    sheet_dictionary["Attributes"] = attributes
+    sheet_dictionary["Skills"] = skills
 
     return sheet_dictionary
 
@@ -26,18 +32,14 @@ def get_sheet(id):
 def get_attributes(attributes_string):
     attributes = {}
 
-    attributes_list = attributes_string.split("|")
+    attributes_type_list = attributes_string.split("|")
     i = 0
-    for attribute in attributes_list[0].split(","):
-        attributes[mental_attributes[i]] = attribute
-        i += 1
-    i = 0
-    for attribute in attributes_list[1].split(","):
-        attributes[physical_attributes[i]] = attribute
-        i += 1
-    i = 0
-    for attribute in attributes_list[2].split(","):
-        attributes[social_attributes[i]] = attribute
+    for attribute_type in attributes_type_list:
+        attributes[types[i]] = {}
+        j = 0
+        for attribute in attribute_type.split(","):
+            attributes[types[i]][attribute_names[i][j]] = attribute
+            j += 1
         i += 1
 
     return attributes
@@ -46,18 +48,14 @@ def get_attributes(attributes_string):
 def get_skills(skills_string):
     skills = {}
 
-    skills_list = skills_string.split("|")
+    skills_type_list = skills_string.split("|")
     i = 0
-    for skill in skills_list[0].split(","):
-        skills[mental_skills[i]] = skill
-        i += 1
-    i = 0
-    for skill in skills_list[1].split(","):
-        skills[physical_skills[i]] = skill
-        i += 1
-    i = 0
-    for skill in skills_list[2].split(","):
-        skills[social_skills[i]] = skill
+    for skill_type in skills_type_list:
+        skills[types[i]] = {}
+        j = 0
+        for skill in skill_type.split(","):
+            skills[types[i]][skill_names[i][j]] = skill
+            j += 1
         i += 1
 
     return skills
